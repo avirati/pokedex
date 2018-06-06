ab45f29225d1c738545c7cd04d8c325b4e7d4e96
Avinash Verma <avinashv@flock.co>
2018-06-06 14:25:48 +0530

    Added favorite functionality

--- app/scripts/common/controllers/pokeCard.controller.js
+++ app/scripts/common/controllers/pokeCard.controller.js
@@ -7,18 +7,27 @@ angular.module('app.controllers')
 
     var PokeCardCtrl = this;
     //Variables
-    angular.extend(PokeCardCtrl, {});
+    angular.extend(PokeCardCtrl, {
+      flags: {
+        loading: false
+      }
+    });
 
     //Functions
     angular.extend(PokeCardCtrl, {
       revealPokemon: function () {
+        PokeCardCtrl.flags.loading = true;
         PokedexDB.getPokemonById($scope.pokemon.id)
         .then(function (researchData) {
           $scope.pokemon.researchData = researchData;
+          PokeCardCtrl.flags.loading = false;
         });
       },
+      toggleSavePokemon: function () {
+        PokedexDB.toggleSavePokemon($scope.pokemon);
+      },
       init: function () {
-        $scope.pokemon = angular.copy($scope.pokemonData)
+        $scope.pokemon = $scope.pokemonData;
       }
     })
 
--- app/scripts/common/services/pokedex.service.js
+++ app/scripts/common/services/pokedex.service.js
@@ -8,11 +8,21 @@ angular.module('app.services')
   function ($localStorage, $q, Queries, FETCH_LIMIT) {
 
     $localStorage.pokedex = $localStorage.pokedex || {};
+
     $localStorage.pokedex.DB = $localStorage.pokedex.DB || [];
     $localStorage.pokedex.indexes = $localStorage.pokedex.indexes || {};
 
+    $localStorage.pokedex.savedPokemon = $localStorage.pokedex.savedPokemon || [];
+
     var DB = $localStorage.pokedex.DB,
-        indexes = $localStorage.pokedex.indexes;
+        indexes = $localStorage.pokedex.indexes,
+        savedPokemon = $localStorage.pokedex.savedPokemon;
+
+    var _markSavedPokemon = function (array) {
+      array.forEach(function (pokemon) {
+        pokemon.saved = savedPokemon.indexOf(pokemon.id) > -1;
+      })
+    };
 
     return {
       getPokemonById: function (id) {
@@ -67,7 +77,9 @@ angular.module('app.services')
         };
 
         if(DB.length > 0 && DB.length >= offset) {  //If total cached pokemon is greater or equals to the offset, serve from cache
-          def.resolve(angular.copy(DB));
+          var _DB = angular.copy(DB);
+          _markSavedPokemon(_DB);
+          def.resolve(_DB);
         }
         else {
           Queries.getPokemon(FETCH_LIMIT, offset)
@@ -86,6 +98,19 @@ angular.module('app.services')
         }
 
         return def.promise;
+      },
+      toggleSavePokemon: function (pokemon) {
+        var index = savedPokemon.indexOf(pokemon.id);
+
+        if(index === -1) {
+          pokemon.saved = true;
+          savedPokemon.push(pokemon.id);
+        }
+        else {
+          pokemon.saved = false;
+          var index = savedPokemon.indexOf(pokemon.id);
+          savedPokemon.splice(index, 1);
+        }
       }
     };
   }]);
